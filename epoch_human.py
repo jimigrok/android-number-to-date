f2ced444fa8d75b05e7242740c27816e2d33b80f
Dietmar Fackelmann <40858305+fackelm2@users.noreply.github.com>
2021-06-06 12:53:53 +0200

    epoch to human readable
    python script to convert epoch timestamp to human readable date

---
+++ epoch_human.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+epoch_human.py: A Python script to convert android timestamps
+Development under Python 3.7 (venv)
+"""
+__author__ = "Dietmar Fackelmann"
+__email__ = "github@fackelmann.org"
+__license__ = "GPLv3"
+
+# NEED MODULE
+# 
+# 2021-06-06 home
+
